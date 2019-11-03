6dca86d66865ff18b660e244a5d610485c401f73
Kgolagano POO <kpoo@c4r11s9.wethinkcode.co.za>
2019-11-03 17:22:27 +0200

    camagru

--- header.php
+++ header.php
@@ -1,30 +1,41 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Camagru</title>
-    <link rel="stylesheet" href="stylesheet.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="css/stylesheet.css">
 </head>
 <body>
-    <header>
-        <a href="#">
-            <img src="images/logo2.png" alt="logo">
-        </a>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-        </ul>
-        <div>
-            <form action="includes/login.inc.php" method="post">
-                <input type="text" name="mailuid" placeholder="Username/E-mail...">
-                <input type="password" name="pwd" placeholder="Password...">
-                <button type="submit" name="login-submit">Login</button>
-            </form>
-            <a href="signup.php">Signup</a>
-            <form action="includes/logout.inc.php" method="post">
-                <button type="submit" name="logout-submit">Logout</button>
-            </form>
-    </header>
+
+<div class="topnav">
+  <a class="active" href="#">Camagru</a>
+</div>
+
+<div>
+  <div class="img">
+      <div class="row">
+        <img src="images/logo2.png" alt="logo">
+      </div>
+  </div>
+  <div class="container">
+      <div class="row">
+        <div class="col-md-4 offset-md-4 form-div">
+                <form action="includes/login.inc.php" method="post">
+                    <div class="form-group">
+                        <input type="text" name="mailuid" placeholder="Username/E-mail..." class="form-control form-contol-lg">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="pwd" placeholder="Password..." class="form-control form-contol-lg">
+                    </div>
+                        <button type="submit" name="login-submit" class="btn btn-primary btn-block btn-lg">Login</button>
+                </form>
+                <br>
+                <p class="text-center" >Not yet a member? <a href="includes/signup.inc.php">Sign Up</a></p>
+        </div>
+      </div>
+</div>
+        
+</div>
+
 </body>
 </html>
